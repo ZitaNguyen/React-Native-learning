7f63092f63f2ad91160c8da80d78466f884d4836
Zita Nguyen <zitanguyen84@gmail.com>
2019-06-24 15:35:16 +0200

    update search

--- components/pages/mainPages/search.page.js
+++ components/pages/mainPages/search.page.js
@@ -1,69 +1,24 @@
-import React, { Component } from "react"
-import { View, Text } from "react-native"
-import { SearchBar } from "react-native-elements"
-import { FlatList, TouchableHighlight } from "react-native-gesture-handler"
-import { WebView } from 'react-native-webview'
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { SearchBar } from 'react-native-elements'
 
-import MyHeader from "../../header"
+import MyHeader from '../../header'
 
-const sourceData = {
-  docSample: require('../../docHTML/docSample.html'),
-  tableSample: require('../../docHTML/tableSample.html'),
-  imageSample: require('../../docHTML/imageSample.html')
-}
-
-const data = [
-                { key: "1", content : sourceData.docSample },
-                { key: "2", content : sourceData.tableSample },
-                { key: "3", content : sourceData.imageSample },
-              ]
-
-
-export default class SearchPage extends Component {
+export default class SearchScreen extends Component {
   state = {
-    search: '',
-    searchResult: []
+      search: '',
+      // searchResults: []
   }
 
-  // searchFilterFunction = text => {
-  //   const searchResult = data.filter( item => {
-  //     return item.content.textContent.includes(text)
-  //   })
-  //   this.setState({search: text, searchResult: searchResult})
-  // }
 
   updateSearch = search => {
-    this.setState({search})
+    this.setState({ search })
+   
   }
 
   onEnd = () => {
     alert(this.state.search)
   }
-
-  // renderSeparator = () => {
-  //   return (
-  //     <View
-  //       style={{
-  //         height: 1,
-  //         backgroundColor: "#CED0CE"
-  //       }}
-  //     />
-  //   )
-  // }
-
-  // renderItem = ({ item }) => (
-  //   <TouchableHighlight
-  //   // onPress={}
-  //   >
-  //     <View>
-  //       {/* <WebView 
-  //         source={item.content}
-  //         style={{height: 750}}
-  //       /> */}
-  //       {/* {this.state.searchResult} */}
-  //     </View>
-  //   </TouchableHighlight>
-  // )
 
   render() {
     return (
@@ -71,20 +26,15 @@ export default class SearchPage extends Component {
         <MyHeader />
         <SearchBar
           placeholder="Taper Ici..."
-          // onChangeText={this.searchFilterFunction}
           onChangeText={this.updateSearch}
           value={this.state.search}
           onEndEditing={this.onEnd}
           lightTheme
           round
         />
-        {/* <FlatList
-          data={data}
-          renderItem={this.renderItem}
-          ItemSeparatorComponent={this.renderSeparator}
-        /> */}
-        {/* <Text>{this.state.searchResult}</Text> */}
+        
       </View>
     )
   }
 }
+
